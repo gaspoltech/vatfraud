d9b2cbc5da8c402fbaa7a82375a9e1eeeb659f48
gaspoltech <88130246+gaspoltech@users.noreply.github.com>
2021-08-10 04:18:26 +0700

    Update vat.py

--- vat.py
+++ vat.py
@@ -106,50 +106,16 @@ def gen_list(npwp):
 
 def main():
     """App by Gaspol"""
-    menu = ["Verdict","VATFraud"]
+    menu = ["Network Analysis","Prediction"]
     
     choice = st.sidebar.selectbox("Select Menu", menu)
         
-    if choice == "Verdict":
+    if choice == "Prediction":
         # st.subheader("Prediction from Model")
 #         st.title("MachineLearning Analytics App")
-        st.subheader("Verdict Prediction with Machine Learning")
-        # iris= Image.open('iris.png')
-
-        model= open("vmodel.pkl", "rb")
-        knn=joblib.load(model)
-
-        st.subheader("Features")
-        #Intializing
-        c1,c2 = st.beta_columns((1,1))
-        with c1:
-            sl = st.number_input(label="FP Lengkap",value=1,min_value=0, max_value=1, step=1)
-            sw = st.number_input(label="FP Tepat Waktu",value=1,min_value=0, max_value=1, step=1)
-            pl = st.number_input(label="Keterangan FP Sesuai",value=0,min_value=0, max_value=1, step=1)
-            dm1 = st.number_input(label="FP Diganti Dibatalkan",value=1,min_value=0, max_value=1, step=1)
-            dm2 = st.number_input(label="FP Tidak Double Kredit",value=1,min_value=0, max_value=1, step=1)
-        with c2:
-            dm0 = st.number_input(label="Lawan PKP",value=1,min_value=0, max_value=1, step=1)
-            dm3 = st.number_input(label="Lawan Disanksi",value=1,min_value=0, max_value=1, step=1)
-            dm4 = st.number_input(label="Lawan Lapor",value=1,min_value=0, max_value=1, step=1)
-            dm5 = st.number_input(label="Minta Tanggung Jawab Lawan",value=1,min_value=0, max_value=1, step=1)
-            pw = st.number_input(label="PPN telah dibayar",value=0,min_value=0, max_value=1, step=1)
-
-        if st.button("Click Here to Classify"):
-            dfvalues = pd.DataFrame(list(zip([sl],[sw],[pl],[pw])),columns =['lengkap', 'tepatwaktu', 'ketsesuai', 'adapembayaran'])
-            input_variables = np.array(dfvalues[['lengkap', 'tepatwaktu', 'ketsesuai', 'adapembayaran']])
-            prediction = knn.predict(input_variables)
-            if prediction == 'ditolak':
-                st.subheader('Prediksi Hasil Verdict')
-                st.title('Permohonan Banding Ditolak')
-            elif prediction =='sebagian':
-                st.subheader('Prediksi Hasil Verdict')
-                st.title('Permohonan Banding Diterima Sebagian')
-            else:
-                st.subheader('Prediksi Hasil Verdict')
-                st.title('Permohonan Banding Diterima Seluruhnya')
-    
-    elif choice == "VATFraud":
+        st.subheader("VAT Fraud Prediction")
+        
+    elif choice == "Network Analysis":
         st.title("VAT Fraud Network Analysis")
         wpsample = [565148728502555,302335091102555,569092136088555]
         st.write(f'Sample WP: {wpsample}')
